39b9a77472559e8a40c8245217974779af2bd533
Allen Riddell <abr@ariddell.org>
2015-07-12 15:35:23 -0400

    Add a test

--- pystb/tests/test_pystb.py
+++ pystb/tests/test_pystb.py
@@ -7,8 +7,11 @@ Tests for `pystb` module.
 
 from pystb.tests import base
 
+import pystb
+
 
 class TestPystb(base.TestCase):
 
     def test_something(self):
-        pass
+        stable = pystb.STable(1, 1, 10, 10, 0)
+        self.assertIsInstance(stable[5, 1], int)
