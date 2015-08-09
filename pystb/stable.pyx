98fb97df5d8b529bdb45ba0089ceb37fb1f24984
Allen Riddell <abr@ariddell.org>
2015-08-09 18:02:09 -0400

    thisptr to _thisptr
    In general, it's pythonic to be liberal in what one exposes as public.
    In this case, however, `_thisptr` really is a private variable.

--- pystb/stable.pyx
+++ pystb/stable.pyx
@@ -50,29 +50,29 @@ cdef class STable:
         max_m (int): Maximum `m` to calculate
     """
     cdef:
-        stable_t * thisptr
+        stable_t * _thisptr
         public unsigned int max_n
         public unsigned int max_m
         public double a
 
     def __cinit__(self, unsigned int max_n, unsigned int max_m, double a):
         self.max_n, self.max_m, self.a = max_n, max_m, a
-        self.thisptr = S_make(10, 10, max_n, max_m, a, S_STABLE|S_UVTABLE|S_FLOAT|S_QUITONBOUND)
-        if not self.thisptr:
+        self._thisptr = S_make(10, 10, max_n, max_m, a, S_STABLE|S_UVTABLE|S_FLOAT|S_QUITONBOUND)
+        if not self._thisptr:
             raise MemoryError("Couldn't allocate memory for STable.")
 
     def S(self, unsigned int n, unsigned int m):
         """:math:`log S^n_{m, a}`"""
         if n > self.max_n or m > self.max_m:
             raise ValueError("Indices exceed table bounds.")
-        return S_S1(self.thisptr, n) if m == 1 else S_S(self.thisptr, n, m)
+        return S_S1(self._thisptr, n) if m == 1 else S_S(self._thisptr, n, m)
 
     def S1(self, unsigned int n, unsigned int m):
         """:math:`log S^n_{1, a}` for n >= 1"""
         assert n >= 1
         if n > self.max_n:
             raise ValueError("Indices exceed table bounds.")
-        return S_S1(self.thisptr, n)
+        return S_S1(self._thisptr, n)
 
     def __getitem__(self, indices):
         """Convenient access, to ``self.S``, ``stable[n, m]`` calls ``stable.S(n, m)``."""
@@ -81,7 +81,7 @@ cdef class STable:
         return self.S(*indices)
 
     def __dealloc__(self):
-        S_free(self.thisptr)
+        S_free(self._thisptr)
 
     def __str__(self):
         cdef:
@@ -91,7 +91,7 @@ cdef class STable:
 
         memset(buf, 0, sizeof(buf))
         fp = tmpfile()
-        S_report(self.thisptr, fp)
+        S_report(self._thisptr, fp)
         rewind(fp)
         num_read = fread(buf, 1, sizeof(buf), fp)  # avoids unused result warning
         fclose(fp)
